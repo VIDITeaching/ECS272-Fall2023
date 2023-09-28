9c4a93cc0cc48360ce9af485dd71742cd05437d6
yhkuo <yskuo@ucdavis.edu>
2023-09-28 12:59:17 -0700

    add more comments

--- Homework2/VanillaJS-Template/app.js
+++ Homework2/VanillaJS-Template/app.js
@@ -2,6 +2,8 @@ import { mountBarChart, BarChart } from './src/Example'
 import { Notes, mountCounter } from './src/notes';
 import './style.css'
 
+// You can manage your layout through CSS, or this template also has materialize library supported.
+// Materialize: https://materializecss.com/getting-started.html
 
 document.querySelector('#app').innerHTML = `
   <div id='main-container' class='d-flex flex-column flex-nowrap'>
