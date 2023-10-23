133c0326a0d14516e7be31f810e7a32413581944
acowe <arcowe12@gmail.com>
2023-10-22 23:38:59 -0700

    add file without git submodule

---
+++ Homework2/accowe/src/App.js
@@ -0,0 +1,121 @@
+import {useState, useEffect} from "react";
+import * as d3 from 'd3';
+import dataset from './pokemon_data.csv'
+import './App.css';
+import DonutChart from './DonutChart.js';
+import ChordDiagram from './ChordDiagram.js';
+import ParallelPlot from './ParallelPlot.js'
+
+function App() {
+    const randVal = Math.floor(Math.random() * 3);
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [dataDetails, setDataDetails] = useState(true);
+    const [circleDetails, setCircleDetails] = useState(true);
+
+    // Since d3.csv is an asynchronous function,
+    // we need to use UseEffect in loading and parsing the data
+    useEffect(() => {
+        d3.csv(dataset).then((d) => {
+            setData(d)
+            setLoading(false)
+        })
+        return () => undefined
+    }, [])
+
+    // Pie/donut chart to show types
+    // Chord graph to show relations of first and second types
+    // Parallel set or coordinate graph showing relationships between body type, primary egg group, and maybe stats
+    // <p>Here, have a {data[(3 * randVal)].Name}!</p>
+  return (
+    <div className="App">
+        <h1 className={"App-header"}>Information Visualization HW #2</h1>
+        <h3>Arisa Cowe</h3>
+        <p> In this (React) web-app, we set up a <b>visualization dashboard</b> to visualize data from a csv dataset. </p>
+        <br/>
+        <h2>Dataset: Pokémon for Data Mining and Machine Learning</h2>
+        {dataDetails &&
+            <div>
+                <p><a href={"https://www.kaggle.com/datasets/alopez247/pokemon/data"}>https://www.kaggle.com/datasets/faisaljanjua0555/best-video-games-of-all-time</a></p>
+                <p><b>Author:</b> ALOPEZ247</p>
+            <p><b>Description:</b> (Almost) all Pokémon stats until generation 6; With the rise of the popularity of machine learning, this is a good opportunity to share a wide database of the even more popular video-game Pokémon by Nintendo, Game freak, and Creatures, originally released in 1996.
+                Pokémon started as a Role Playing Game (RPG), but due to its increasing popularity, its owners ended up producing many TV series, manga comics, and so on, as well as other types of video-games (like the famous Pokémon Go!).
+                This dataset is focused on the stats and features of the Pokémon in the RPGs. Until now (08/01/2017) seven generations of Pokémon have been published. All in all, this dataset does not include the data corresponding to the last generation, since 1) I created the databased when the seventh generation was not released yet, and 2) this database is a modification+extension of the database "721 Pokemon with stats" by Alberto Barradas, which does not include (of course) the latest generation either.
+                This database includes 21 variables per each of the 721 Pokémon of the first six generations, plus the Pokémon ID and its name.
+            </p>
+            <p>Full details of the 21 variables are available on the kaggle page. The ones we will be focusing on in this dashboard are:</p>
+            <ul>
+                <li><b>Type_1:</b> Primary type</li>
+                <li><b>Type_2:</b> Second type, in case the Pokémon has it.</li>
+                <li><b>Height_m:</b> Height of the Pokémon, in meters.</li>
+                <li><b>Weight_kg:</b> Weight of the Pokémon, in kilograms.</li>
+                <li><b>HP:</b> Base Health Points.</li>
+                <li><b>Attack:</b> Base Attack.</li>
+                <li><b>Defense:</b> Base Defense.</li>
+                <li><b>Sp_Atk:</b> Base Special Attack.</li>
+                <li><b>Sp_Def:</b> Base Special Defense.</li>
+                <li><b>Speed:</b> Base Speed.</li>
+            </ul> </div>}
+
+        <p onClick={(e) => setDataDetails(!dataDetails)}> {dataDetails ? "[-] Close": "[+] Expand"} </p>
+
+        <br/>
+
+        <h1>Visualization Dashboard</h1>
+
+        <p>Before generating the visualizations, we first have to parse in the csv data.
+            While the csv file is included with the project file, it cannot be used as is. Hence, we will use d3's <span>csv</span> function to parse it.
+            (In React, this would be done within <span>useEffect</span> as it is an asynchronous operation.
+            See React's documentation on <a href={"https://react.dev/reference/react/useEffect"}>useEffect</a> and Benny Au's <a href={"https://www.pluralsight.com/guides/load-remote-chart-data-for-d3.js-in-a-react-app"}>guide</a> and  for more details.)
+            The status below is the parsing status. Once the data has been loaded in, you'll be able to see the entirety of the dashboard below
+        </p>
+
+        <h3><b>Status:</b> {loading ? "Loading..." : "Loaded"} </h3>
+
+
+
+        <br/>
+        {loading && <p>Dataset loading...</p>}
+        {!loading &&
+            <div>
+                <h2>Seeing in Circles </h2>
+                <p> Below are two circular visualizations side-by-side, showing the <b>distribution of Pokemon</b> across their <b>18 different elemental types</b>. </p>
+                <p> On the left is a <b>donut chart</b> showing the numerical distribution of 721 pokemon across their primary typings (Type_1).
+                    For context, Many pokemon, especially those in the later generations, have secondary types (i.e. they have two types) but we focus on primary typing to avoid overcounting.
+                </p>
+                <p>On the right is a <b>chord diagram</b> that shows the (directed) relationships between the pokemons' primary and secondary typings.
+                    In particular, the outer arc denotes the number of pokemon with primary type X, similarly to the donut chart,
+                    while the inner chord denotes the number of pokemon with secondary type Y (target) that have primary type X (source). </p>
+                <div className={"dashboard"}>
+                    <DonutChart data={data}/>
+                    <svg width={100} height={600}/>
+                    <ChordDiagram data={data}/>
+                </div>
+                <br/>
+
+                <h2>Pokemon Physique, Power, & Parallel Plot</h2>
+                <p>Below is a <b>parallel plot</b> showing the broader relationships between physical characteristics and stats across the 721 pokemon.
+                    The lines are colored by the individual pokemon's primary type per the color scheme established in the previous section.</p>
+                <ParallelPlot data={data}/>
+                <br/>
+                <h2>Closing Remarks</h2>
+                <p>While the advanced visualizations are interesting to look at as they are, I admit they can be hard to look at as well.
+                    In the case of the parallel plot, it can be hard to identify the trends between variables between individual instances of pokemon with so many lines displayed at once. At least some of the general trends between height, weight, and stats can be easily observed, such as the zig-zags between the stats or the mass of lines converging around
+                    a range of values going from height to weight. In contrast, the chord diagram proves chaotic to look at. With so many types and, in turn, so many possible 2-permutations of types, there ends up being many overlapping ribbons, some of which really narrow out at the end. As a result, it can be hard to see some of the minor type relationships
+                    without zooming in or reducing the opacity of the other ribbons.
+                </p>
+                <p>For next time, I would like to add interactivity to these advanced visualizations so that it will be easier to filter visual information and see the nuances in trends and relationships.</p>
+            </div>
+        }
+        <h3>References:</h3>
+        <ul>
+            <li><b>Donut chart:</b> <a href={"https://observablehq.com/@d3/donut-chart/2?intent=fork"}>Donut Chart Notebook</a> by Observable, <a href={"https://gist.github.com/dbuezas/9306799"}>Pie Chart Labels</a> by dbuezas on GitHub</li>
+            <li><b>Chord diagram:</b> <a href={"https://d3js.org/d3-chord/chord"}>d3-chords</a> + <a href={"https://observablehq.com/@d3/chord-diagram"}>Chord Diagram I Notebook</a> by Observable</li>
+            <li><b>Parallel Plot:</b> <a href={"https://d3-graph-gallery.com/graph/parallel_custom.html"}>Parallel coordinates chart with color effect</a> by Yan Holtz on D3.js Graph Gallery</li>
+        </ul>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Homework2/accowe/src/ChordDiagram.js
@@ -0,0 +1,287 @@
+import {useEffect, useState} from "react";
+import * as d3 from "d3";
+
+function processData(data){
+    // Sort data by primary type (Type_1) X and track the number of pokemon with secondary type (Type_2) Y for each of the 18 types.
+    // This will result in 18 x 18 data matrix with [i,j] corresponding to [Type_1_index, Type_2_index]
+    // Color is included for future reference when constructing chart
+
+    // Note: I re-ordered the object order, hence the weird index ordering. I wanted to order the chord diagram
+    // path elements in the same order as the donut chart but could not figure out how to do so automatically.
+
+    const types = {
+        Flying: {index:4, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#A98FF3"},
+        Fairy: {index:17, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#D685AD"},
+        Steel: {index:14, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#B7B7CE"},
+        Ice: {index:11, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#96D9D6"},
+        Ghost: {index:13, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#735797"},
+        Dragon: {index:15, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#6F35FC"},
+        Fighting: {index:5, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#C22E28"},
+        Poison: {index:6, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#A33EA1"},
+        Dark: {index:16, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#705746"},
+        Ground: {index:8, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#E2BF65"},
+        Electric: {index:7, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#F7D02C"},
+        Rock: {index:9, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#B6A136"},
+        Fire: {index:1, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#EE8130"},
+        Psychic: {index:10, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#F95587"},
+        Bug: {index:12, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#A6B91A"},
+        Grass: {index:3, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#7AC74C"},
+        Normal: {index:0, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#A8A77A"},
+        Water: {index:2, noPokemon:0, noPokePerType: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], color:"#6390F0"},
+    }
+
+    // Sort data by primary type (Type_1) and track the number of pokemon for each.
+    // If select pokemon d has second type, i.e. Type_2 is non-empty string, then increment value at array index
+    // coresponding to that type
+    data.forEach(d => {
+        switch (d.Type_1) {
+            case "Normal":
+                types.Normal.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Normal.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Fire":
+                types.Fire.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Fire.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Water":
+                types.Water.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Water.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Grass":
+                types.Grass.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Grass.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Flying":
+                types.Flying.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Flying.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Fighting":
+                types.Fighting.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Fighting.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Poison":
+                types.Poison.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Poison.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Electric":
+                types.Electric.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Electric.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Ground":
+                types.Ground.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Ground.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Rock":
+                types.Rock.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Rock.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Psychic":
+                types.Psychic.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Psychic.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Ice":
+                types.Ice.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Ice.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Bug":
+                types.Bug.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Bug.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Ghost":
+                types.Ghost.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Ghost.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Steel":
+                types.Steel.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Steel.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Dragon":
+                types.Dragon.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Dragon.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Dark":
+                types.Dark.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Dark.noPokePerType[types[d.Type_2].index] += 1
+                break
+            case "Fairy":
+                types.Fairy.noPokemon += 1
+                if (d.Type_2.length > 0)
+                    types.Fairy.noPokePerType[types[d.Type_2].index] += 1
+                break
+        }
+    })
+
+    // Return two arrays with our data to be used for our chart
+    // typeData will be the comprehensive data from above as an array
+    // matData will be the matrix corresponding to the from-to values for the chord diagram
+    const typeData = []
+    const matData = []
+    Object.keys(types).forEach(t => {
+        types[t].name = t;
+        typeData.push(types[t]);
+        matData.push(types[t].noPokePerType)
+    });
+
+    return [typeData, matData]
+
+}
+
+function ChordDiagram(props){
+    // Creates and returns the chord diagram SVG to be displayed
+
+    // process data
+    const processedData = processData(props.data)[0]
+    const matrixData = processData(props.data)[1]
+
+    // Set up dimensional constants for svg
+    const width = 600, height = 600;
+    const outerRadius = Math.min(width, height) * 0.5 - 30;
+    const innerRadius = outerRadius - 20;
+
+
+    // Create chord diagram SVG within useEffect
+    useEffect(() => {
+
+        function groupTicks(d, step) {
+            // Establishes tick positioning and valuation for the chord diagram
+            const k = (d.endAngle - d.startAngle) / d.value;
+            return d3.range(0, d.value, step).map(value => {
+                return {value: value, angle: value * k + d.startAngle};
+            });
+        }
+
+        // Some more tick formatting constants
+        const sum = d3.sum(matrixData.flat());
+        const tickStep = d3.tickStep(0, sum, 100);
+        const tickStepMajor = d3.tickStep(0, sum, 20);
+        const formatValue = d3.formatPrefix(",.0", tickStep);
+
+        // Chord diagram's ribbon group setup
+        const chord = d3.chord()
+            .padAngle(2 / innerRadius)
+            .sortSubgroups(d3.descending);
+
+        // Chord diagram's arc dimensions
+        const arc = d3.arc()
+            .outerRadius(outerRadius)
+            .innerRadius(innerRadius)
+
+        // The ribbons that connect two arc-sections of the chord diagram
+        const ribbon = d3.ribbon()
+            .radius(innerRadius);
+
+        // Set up the SVG display
+        // Link d3 svg instance with the svg element whose id is "chord"
+        let svg = d3.select('#chord')
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("viewBox", [-width / 2 - 20, -height / 2 - 20 , width * 1.1, height * 1.1])
+            .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
+
+        // Set up instance of chords with matrix data
+        const chords = chord(matrixData)
+
+        // Append chord grouping to svg
+        const group = svg.append("g")
+            .selectAll()
+            .data(chords.groups)
+            .join("g");
+
+        // Construct the arc segments + circle for chord diagram
+        group.append("path")
+            .attr("fill", d => processedData[d.index].color)
+            .attr("d", arc)
+            .append("title")
+            .text(d => `${d.value.toLocaleString("en-US")} ${processedData[d.index].name}`);
+
+        // Append ticks to circle
+        const groupTick = group.append("g")
+            .selectAll()
+            .data(d => groupTicks(d, tickStep))
+            .join("g")
+            .attr("transform", d => `rotate(${d.angle * 180 / Math.PI - 90}) translate(${outerRadius},0)`);
+
+        // Draw out tick lines
+        groupTick.append("line")
+            .attr("stroke", "currentColor")
+            .attr("x2", 6);
+
+        // Add numerical text to major ticks (i.e. ticks denoting numbers at every instance of 20)
+        groupTick
+            .filter(d => d.value % tickStepMajor === 0)
+            .append("text")
+            .attr("x", 8)
+            .attr("dy", ".35em")
+            .attr("transform", d => d.angle > Math.PI ? "rotate(180) translate(-16)" : null)
+            .attr("text-anchor", d => d.angle > Math.PI ? "end" : null)
+            .text(d => formatValue(d.value));
+
+        // Add arc-section titles to denote types and their direction
+        group.select("text")
+            .attr("font-weight", "bold")
+            .text(function(d) {
+                return this.getAttribute("text-anchor") === "end"
+                    ? `↑ ${processedData[d.index].name}`
+                    : `${processedData[d.index].name} ↓`;
+            });
+
+        // Add the ribbons to the chord diagram
+        svg.append("g")
+            .selectAll()
+            .data(chords)
+            .join("path")
+            .attr("id", d => "c" + processedData[d.source.index].name)
+            .attr("d", ribbon)
+            .attr("fill", d => processedData[d.source.index].color)
+            .attr("fill-opacity",0.5)
+            .append("title")
+            .text(d =>
+                `${d.source.value.toLocaleString("en-US")} 
+                ${processedData[d.source.index].name} → ${processedData[d.target.index].name}
+                ${d.source.index !== d.target.index ? `\n${d.target.value.toLocaleString("en-US")} ${processedData[d.target.index].name} → ${processedData[d.source.index].name}` : ``}`);
+
+        // Add title text
+        svg.append("text")
+            .attr("x", -320)
+            .attr("y", -280)
+            .attr("font-weight", "bold")
+            .attr("font-size", 15)
+            .text("No. Pokemon with Primary Type X ");
+
+        svg.append("text")
+            .attr("x", -320)
+            .attr("y", -260)
+            .attr("font-weight", "bold")
+            .attr("font-size", 15)
+            .text("and Secondary Type Y");
+
+
+
+    })
+
+    // Return the svg chart
+    return(
+            <svg width={width} height={height} id={"chord"} />
+    )
+
+}
+
+export default ChordDiagram;
