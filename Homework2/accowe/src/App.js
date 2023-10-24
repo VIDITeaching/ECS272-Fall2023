cbdf401a01aae3f60773f42bc7499847bdfc0419
acowe <arcowe12@gmail.com>
2023-10-24 13:23:34 -0700

    changed plot sizes fro dashboard display

--- Homework2/accowe/src/App.js
+++ Homework2/accowe/src/App.js
@@ -10,8 +10,8 @@ function App() {
     const randVal = Math.floor(Math.random() * 3);
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [dataDetails, setDataDetails] = useState(true);
-    const [circleDetails, setCircleDetails] = useState(true);
+    const [dataDetails, setDataDetails] = useState(false);
+    const [detailView, setDetailView] = useState(false);
 
     // Since d3.csv is an asynchronous function,
     // we need to use UseEffect in loading and parsing the data
@@ -29,12 +29,10 @@ function App() {
     // <p>Here, have a {data[(3 * randVal)].Name}!</p>
   return (
     <div className="App">
-        <h1 className={"App-header"}>Information Visualization HW #2</h1>
-        <h3>Arisa Cowe</h3>
-        <p> In this (React) web-app, we set up a <b>visualization dashboard</b> to visualize data from a csv dataset. </p>
-        <br/>
-        <h2>Dataset: Pokémon for Data Mining and Machine Learning</h2>
-        {dataDetails &&
+        <div className={"App-header"}><h1>Information Visualization HW #2: Visualization Dashboard</h1> <div></div> <p>Details: Off</p></div>
+        <h3 style={{marginBottom:10 +"px", marginTop:10 + "px"}}>Arisa Cowe</h3>
+        <h3 style={{marginTop:0 +"px"}}>Dataset: Pokémon for Data Mining and Machine Learning</h3>
+        {(detailView && dataDetails) &&
             <div>
                 <p><a href={"https://www.kaggle.com/datasets/alopez247/pokemon/data"}>https://www.kaggle.com/datasets/faisaljanjua0555/best-video-games-of-all-time</a></p>
                 <p><b>Author:</b> ALOPEZ247</p>
@@ -56,55 +54,56 @@ function App() {
                 <li><b>Sp_Def:</b> Base Special Defense.</li>
                 <li><b>Speed:</b> Base Speed.</li>
             </ul> </div>}
+        {detailView && <div>
+            <p onClick={(e) => setDataDetails(!dataDetails)}> {dataDetails ? "[-] Close": "[+] Expand"} </p>
+            <p>Before generating the visualizations, we first have to parse in the csv data.
+                While the csv file is included with the project file, it cannot be used as is. Hence, we will use d3's <span>csv</span> function to parse it.
+                (In React, this would be done within <span>useEffect</span> as it is an asynchronous operation.
+                See React's documentation on <a href={"https://react.dev/reference/react/useEffect"}>useEffect</a> and Benny Au's <a href={"https://www.pluralsight.com/guides/load-remote-chart-data-for-d3.js-in-a-react-app"}>guide</a> and  for more details.)
+                The status below is the parsing status. Once the data has been loaded in, you'll be able to see the entirety of the dashboard below
+            </p>
+            <h3 style={{marginBottom:0}}><b>Status:</b> {loading ? "Loading..." : "Loaded"} </h3>
+        </div>}
 
-        <p onClick={(e) => setDataDetails(!dataDetails)}> {dataDetails ? "[-] Close": "[+] Expand"} </p>
-
-        <br/>
-
-        <h1>Visualization Dashboard</h1>
-
-        <p>Before generating the visualizations, we first have to parse in the csv data.
-            While the csv file is included with the project file, it cannot be used as is. Hence, we will use d3's <span>csv</span> function to parse it.
-            (In React, this would be done within <span>useEffect</span> as it is an asynchronous operation.
-            See React's documentation on <a href={"https://react.dev/reference/react/useEffect"}>useEffect</a> and Benny Au's <a href={"https://www.pluralsight.com/guides/load-remote-chart-data-for-d3.js-in-a-react-app"}>guide</a> and  for more details.)
-            The status below is the parsing status. Once the data has been loaded in, you'll be able to see the entirety of the dashboard below
-        </p>
-
-        <h3><b>Status:</b> {loading ? "Loading..." : "Loaded"} </h3>
-
-
-
-        <br/>
         {loading && <p>Dataset loading...</p>}
         {!loading &&
             <div>
-                <h2>Seeing in Circles </h2>
-                <p> Below are two circular visualizations side-by-side, showing the <b>distribution of Pokemon</b> across their <b>18 different elemental types</b>. </p>
-                <p> On the left is a <b>donut chart</b> showing the numerical distribution of 721 pokemon across their primary typings (Type_1).
-                    For context, Many pokemon, especially those in the later generations, have secondary types (i.e. they have two types) but we focus on primary typing to avoid overcounting.
-                </p>
-                <p>On the right is a <b>chord diagram</b> that shows the (directed) relationships between the pokemons' primary and secondary typings.
-                    In particular, the outer arc denotes the number of pokemon with primary type X, similarly to the donut chart,
-                    while the inner chord denotes the number of pokemon with secondary type Y (target) that have primary type X (source). </p>
+                {detailView && <div>
+                    <h2>Seeing in Circles </h2>
+                    <p> Below are two circular visualizations side-by-side, showing the <b>distribution of Pokemon</b> across their <b>18 different elemental types</b>. </p>
+                    <p> On the left is a <b>donut chart</b> showing the numerical distribution of 721 pokemon across their primary typings (Type_1).
+                        For context, Many pokemon, especially those in the later generations, have secondary types (i.e. they have two types) but we focus on primary typing to avoid overcounting.
+                    </p>
+                    <p>On the right is a <b>chord diagram</b> that shows the (directed) relationships between the pokemons' primary and secondary typings.
+                        In particular, the outer arc denotes the number of pokemon with primary type X, similarly to the donut chart,
+                        while the inner chord denotes the number of pokemon with secondary type Y (target) that have primary type X (source). </p>
+                </div>}
+
                 <div className={"dashboard"}>
                     <DonutChart data={data}/>
-                    <svg width={100} height={600}/>
+                    <svg width={100} height={360}/>
                     <ChordDiagram data={data}/>
                 </div>
-                <br/>
 
-                <h2>Pokemon Physique, Power, & Parallel Plot</h2>
-                <p>Below is a <b>parallel plot</b> showing the broader relationships between physical characteristics and stats across the 721 pokemon.
-                    The lines are colored by the individual pokemon's primary type per the color scheme established in the previous section.</p>
+                {detailView && <div>
+                    <br/>
+                    <h2>Pokemon Physique, Power, & Parallel Plot</h2>
+                    <p>Below is a <b>parallel plot</b> showing the broader relationships between physical characteristics and stats across the 721 pokemon.
+                        The lines are colored by the individual pokemon's primary type per the color scheme established in the previous section.</p>
+                </div>}
+
                 <ParallelPlot data={data}/>
                 <br/>
-                <h2>Closing Remarks</h2>
-                <p>While the advanced visualizations are interesting to look at as they are, I admit they can be hard to look at as well.
-                    In the case of the parallel plot, it can be hard to identify the trends between variables between individual instances of pokemon with so many lines displayed at once. At least some of the general trends between height, weight, and stats can be easily observed, such as the zig-zags between the stats or the mass of lines converging around
-                    a range of values going from height to weight. In contrast, the chord diagram proves chaotic to look at. With so many types and, in turn, so many possible 2-permutations of types, there ends up being many overlapping ribbons, some of which really narrow out at the end. As a result, it can be hard to see some of the minor type relationships
-                    without zooming in or reducing the opacity of the other ribbons.
-                </p>
-                <p>For next time, I would like to add interactivity to these advanced visualizations so that it will be easier to filter visual information and see the nuances in trends and relationships.</p>
+                {detailView && <div>
+                    <h2>Closing Remarks</h2>
+                    <p>While the advanced visualizations are interesting to look at as they are, I admit they can be hard to look at as well.
+                        In the case of the parallel plot, it can be hard to identify the trends between variables between individual instances of pokemon with so many lines displayed at once. At least some of the general trends between height, weight, and stats can be easily observed, such as the zig-zags between the stats or the mass of lines converging around
+                        a range of values going from height to weight. In contrast, the chord diagram proves chaotic to look at. With so many types and, in turn, so many possible 2-permutations of types, there ends up being many overlapping ribbons, some of which really narrow out at the end. As a result, it can be hard to see some of the minor type relationships
+                        without zooming in or reducing the opacity of the other ribbons.
+                    </p>
+                    <p>For next time, I would like to add interactivity to these advanced visualizations so that it will be easier to filter visual information and see the nuances in trends and relationships.</p>
+                </div>}
+
             </div>
         }
         <h3>References:</h3>
