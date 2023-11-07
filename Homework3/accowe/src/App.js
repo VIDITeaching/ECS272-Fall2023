7ef87200b302b53c299787ac10d6a6554d8d4d20
acowe <arcowe12@gmail.com>
2023-11-06 16:56:18 -0800

    uplodaing HW 3 to repo

---
+++ Homework3/accowe/src/App.js
@@ -0,0 +1,114 @@
+import {useState, useEffect} from "react";
+import * as d3 from 'd3';
+import dataset from './pokemon_data.csv'
+import './App.css';
+import DonutChart from './DonutChart.js';
+import ChordDiagram from './ChordDiagram.js';
+import ParallelPlot from './ParallelPlot.js'
+
+const typeColors = {
+    Normal: "#A8A77A",
+    Fire: "#EE8130",
+    Water: "#6390F0",
+    Grass: "#7AC74C",
+    Flying: "#A98FF3",
+    Fighting: "#C22E28",
+    Poison: "#A33EA1",
+    Electric: "#F7D02C",
+    Ground: "#E2BF65",
+    Rock: "#B6A136",
+    Psychic: "#F95587",
+    Ice: "#96D9D6",
+    Bug: "#A6B91A",
+    Ghost: "#735797",
+    Steel: "#B7B7CE",
+    Dragon: "#6F35FC",
+    Dark: "#705746",
+    Fairy: "#D685AD",
+}
+function App() {
+    const randVal = Math.floor(Math.random() * 3);
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [detailView, setDetailView] = useState(false);
+    const [selectType, setSelectType] =  useState("")
+
+    function resetCharts(){
+        d3.selectAll(".piePieces").transition().duration(350).attr("fill-opacity", 1)
+        d3.selectAll(".pieText").transition().duration(350).attr("fill-opacity", 1)
+        d3.selectAll(".polylines").transition().duration(350).attr("stroke-opacity", 1)
+        d3.selectAll(".ribbons").transition().duration(350).attr("fill-opacity", 0.7).attr("stroke-opacity", 1)
+        d3.selectAll(".pplines").raise()
+        d3.selectAll(".pplines").transition().duration(350).style("stroke", function (){
+            let typeStr = this.id.substring(3)
+            return typeColors[typeStr]
+        }).style("opacity", 0.4)
+        d3.selectAll(".axis").raise();
+    }
+
+    // Since d3.csv is an asynchronous function,
+    // we need to use UseEffect in loading and parsing the data
+    useEffect(() => {
+        d3.csv(dataset).then((d) => {
+            setData(d)
+            setLoading(false)
+        })
+        return () => undefined
+    }, [])
+
+    // Pie/donut chart to show types
+    // Chord graph to show relations of first and second types
+    // Parallel set or coordinate graph showing relationships between body type, primary egg group, and maybe stats
+    // <p>Here, have a {data[(3 * randVal)].Name}!</p>
+  return (
+    <div className="App">
+        <h1 className={"App-header"}>Interactive Visualization Dashboard for Pokemon Data</h1>
+        {selectType}
+        {loading && <p>Dataset loading...</p>}
+        {!loading &&
+            <div>
+                <div className={"dashboard"}>
+                    <DonutChart data={data} resetCharts={resetCharts}/>
+                    <svg width={100} height={410} />
+                    <ChordDiagram data={data} resetCharts={resetCharts}/>
+                </div>
+                <ParallelPlot data={data} resetCharts={resetCharts}/>
+            </div>
+        }
+        {detailView &&
+            <div className={"details"}>
+                <h2>Dataset: Pokémon for Data Mining and Machine Learning</h2>
+                <p><a href={"https://www.kaggle.com/datasets/alopez247/pokemon/data"}>https://www.kaggle.com/datasets/faisaljanjua0555/best-video-games-of-all-time</a></p>
+
+                <h2>Description:</h2>
+                <p> This interactive visualization aims to show the <b>distribution of 721 Pokemon</b> across their <b>18 (primary) elemental types</b>,
+                    the distribution of secondary types across each primary type, and the relation between a pokemon's type and their physical characteristics (height and weight) and battle stats (HP, attack, defense, special attack, special fefense, and speed).</p>
+
+            <p> The two circular visualizations each show the <b>distribution of Pokemon</b> across <b>18 different elemental types</b>. </p>
+            <p> In particular, the <b>donut chart</b> shows the numerical distribution of <b>721 pokemon</b> across their <b>primary typings</b> (Type_1).
+                For context, Many pokemon, especially those in the later generations, have secondary types (i.e. they have two types) but we focus on primary typing to avoid overcounting.
+            </p>
+
+            <p>The <b>chord diagram</b> on the right shows the (directed) relationships between the <b>pokemons' primary and secondary typings</b> for
+                the <b>350 pokemon</b> that have two types. In particular, the outer arc denotes the number of pokemon with primary type X, similarly to the donut chart,
+                while the inner chord denotes the number of pokemon with secondary type Y (target) that have primary type X (source). </p>
+
+            <p> Finally, the <b>parallel plot</b> at the bottom shows the broader relationships between <b>type, physical characteristics, and battle stats</b> across the <b>721 pokemon</b>.
+                The lines are colored by the individual pokemon's primary type per the color scheme established in the donut chart.</p>
+
+                <h3>References:</h3>
+                <ul>
+                    <li><b>Donut chart:</b> <a href={"https://observablehq.com/@d3/donut-chart/2?intent=fork"}>Donut Chart Notebook</a> by Observable, <a href={"https://gist.github.com/dbuezas/9306799"}>Pie Chart Labels</a> by dbuezas on GitHub</li>
+                    <li><b>Chord diagram:</b> <a href={"https://d3js.org/d3-chord/chord"}>d3-chords</a> + <a href={"https://observablehq.com/@d3/chord-diagram"}>Chord Diagram I Notebook</a> by Observable</li>
+                    <li><b>Parallel Coordinates:</b> <a href={"https://d3-graph-gallery.com/graph/parallel_custom.html"}>Parallel coordinates chart with color effect</a> by Yan Holtz on D3.js Graph Gallery</li>
+                    <li><b>Parallel Coordinate Brushing:</b> <a href={"https://observablehq.com/@d3/brushable-parallel-coordinates"}>Brushable parallel coordinates</a> by Kerry Rodden on Observable</li>
+                    <li><b>D3 Event Listeners:</b> <a href={"https://gramener.github.io/d3js-playbook/events.html"}>Event Listeners</a> in D3.js Playbook by Pratap Vardhan </li>
+                </ul>
+        </div>}
+        <p className={"details"} onClick={(e) => setDetailView(!detailView)}> {detailView ?  "[-] Close" : "[+] Additional details"}</p>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Homework3/accowe/src/ParallelPlot.js
@@ -0,0 +1,181 @@
+import {useEffect, useState} from "react";
+import * as d3 from "d3";
+import {click} from "@testing-library/user-event/dist/click";
+
+function processData(data){
+    // Organize data values in arrays that can directly be used to construct parallel plot axis
+
+    let pData = {
+        Height_m: [],
+        Weight_kg: [],
+        HP: [],
+        Attack: [],
+        Defense: [],
+        Sp_Atk: [],
+        Sp_Def: [],
+        Speed: [],
+        Total: [],
+    }
+
+    // Push numerical value into corresponding variable array
+    data.forEach(d => {
+        pData.Height_m.push(d.Height_m)
+        pData.Weight_kg.push(d.Weight_kg)
+        pData.HP.push(d.HP)
+        pData.Attack.push(d.Attack)
+        pData.Defense.push(d.Defense)
+        pData.Sp_Atk.push(d.Sp_Atk)
+        pData.Sp_Def.push(d.Sp_Def)
+        pData.Speed.push(d.Speed)
+        pData.Total.push(d.Total)
+    })
+
+    return pData
+}
+function ParallelPlot(props){
+    // process data for parallel plot
+    const processedData = processData(props.data)
+
+    // Set up dimensional constants for svg
+    const margin = {top: 30, right: 50, bottom: 10, left: 10},
+        width = 1750,
+        height = 400;
+
+    // Set up type colors for colored lines based on type
+    const typeColors = {
+        Normal: "#A8A77A",
+        Fire: "#EE8130",
+        Water: "#6390F0",
+        Grass: "#7AC74C",
+        Flying: "#A98FF3",
+        Fighting: "#C22E28",
+        Poison: "#A33EA1",
+        Electric: "#F7D02C",
+        Ground: "#E2BF65",
+        Rock: "#B6A136",
+        Psychic: "#F95587",
+        Ice: "#96D9D6",
+        Bug: "#A6B91A",
+        Ghost: "#735797",
+        Steel: "#B7B7CE",
+        Dragon: "#6F35FC",
+        Dark: "#705746",
+        Fairy: "#D685AD",
+    }
+
+    // Set up domain names for parallel plot
+    const dimensions = ["Height_m", "Weight_kg", "HP", "Attack", "Defense", "Sp_Atk", "Sp_Def", "Speed", "Total"]
+
+    // Create parallel plot SVG within useEffect
+    useEffect(() => {
+
+        // Set up the SVG display
+        // Link d3 svg instance with the svg element whose id is "parallel"
+        let svg = d3.select("#parallel")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .attr("viewBox", [-height/6, -40, 1.35 * width, 1.35 * height])
+            .attr("style", "max-width: 100%; height: auto;")
+            .append("g")
+            .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+
+        // Builds the Y scale; It will find the best position for each Y axis
+        let y = {}
+        for (let i in dimensions) {
+            let name = dimensions[i]
+            y[name] = d3.scaleLinear()
+                .domain([Math.min(...processedData[name]), Math.max(...processedData[name])])
+                .range([height, 0])
+        }
+
+        // Builds the X scale; It find the best position for each Y axis
+        let x = d3.scalePoint()
+            .range([0, width])
+            .padding(1)
+            .domain(dimensions);
+
+        function makePath(d) {
+            // creates line's path from initial position on one axis to final position on the next axis
+            return d3.line()(dimensions.map(function(p)
+            {
+                return [x(p), y[p](d[p])];
+            }));
+        }
+
+        const path = svg
+            .selectAll("myPath")
+            .data(props.data)
+            .enter()
+            .append("path")
+            .attr("class", "pplines") // 2 classes for each line: 'line' and the group name
+            .attr("id", function (d) { return "ppl" + d.Type_1 } )
+            .attr("d",  makePath)
+            .style("fill", "none" )
+            .style("stroke", function(d){ return(typeColors[d.Type_1])} )
+            .style("opacity", 0.4)
+
+        const axes = svg.selectAll("myAxis")
+            // For each dimension of the dataset, add a 'g' element:
+            .data(dimensions).enter()
+            .append("g")
+            .attr("class", "axis")
+            // Translate this element to its right position on the x axis
+            .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
+            // Build the axis with the call function
+            .each(function(d) { d3.select(this).call(d3.axisLeft().ticks(5).scale(y[d])); })
+            // Add axis title
+            .append("text")
+            .style("text-anchor", "middle")
+            .style("font-weight", "bold")
+            .attr("y", -9)
+            .text(function(d) { return d; })
+            .style("fill", "black");
+
+        const deselectedColor = "#e6e6e6";
+        const brushWidth = 20;
+        const brush = d3.brushY()
+            .extent([[-brushWidth/2, 0], [brushWidth/2,height]])
+            .on("start brush end",
+                brushed);
+
+        d3.selectAll(".axis").call(brush);
+
+        const selections = new Map();
+        function brushed({selection}, key) {
+            if (selection === null) selections.delete(key);
+            else selections.set(key, selection.map(y[key].invert));
+            const selected = [];
+            path.each(function(d) {
+                const active = Array.from(selections).every(([key, [max,min]]) => d[key] >= min && d[key] <= max)
+                d3.select(this).style("stroke", active ? typeColors[d.Type_1] : deselectedColor);
+                if (active) {
+                    d3.select(this).raise();
+                    selected.push(d);
+                    d3.selectAll(".axis").raise();
+                }
+            });
+            d3.selectAll(".piePieces").attr("fill-opacity", 1)
+            d3.selectAll(".pieText").attr("fill-opacity", 1)
+            d3.selectAll(".polylines").attr("stroke-opacity", 1)
+            d3.selectAll(".ribbons").attr("fill-opacity", 0.7).attr("stroke-opacity", 1)
+            svg.property("value", selected).dispatch("input");
+        }
+
+        // Add chart title
+        svg.append("text")
+            .attr("x",125)
+            .attr("y", -50)
+            .attr("font-weight", "bold")
+            .attr("font-size", 20)
+            .text("Parallel Plot for Pokemon Physical Characteristic and Battle Stat Data");
+
+    })
+
+    // Return the svg chart
+    return(
+            <svg width={width} height={height} id={"parallel"} />
+    )
+}
+export default ParallelPlot;
