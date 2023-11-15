a6d61dd4829eae91a363ec766055e0e79513ac15
Kunal Mundada <mundadakunal25@gmail.com>
2023-11-15 15:02:16 -0800

    Shifted color schemes for charts into global.js

--- Homework3/KunalMundada/src/globals.js
+++ Homework3/KunalMundada/src/globals.js
@@ -1,7 +1,32 @@
+import * as d3 from "d3";
 import { updateBubbleChart } from "./bubblechart";
 import { updateSunburstChart } from "./sunburst";
 
+let country_name;
+
 export function updateCharts(key) {
+  country_name = key;
   updateBubbleChart(key);
   updateSunburstChart(key);
+  country_name = key;
 }
+
+ // export const mapColorScheme = d3
+  //   .scaleSequentialLog()
+  //   .domain(d3.extent(data, (d) => d.market_size))
+  //   .nice()
+  //   .interpolator(d3.interpolateBlues);
+
+export const mapColorScheme = d3
+.scaleThreshold()
+.domain([10000, 100000, 1000000, 10000000])
+.range(["#F2B279", "#F2CCB6", "#F2E4DC", "#0D0D0D"])
+// .range(["#0D0D0D", "#F2E4DC", "#F2CCB6", "#F2B279"])
+.unknown("#EBEBF2"); // #EBEBF2 or #FEFFFE
+
+export const scatterColorScheme = d3
+.scaleOrdinal()
+.domain([country_name, "ROW"])
+.range(["#F2B279", "#0D0D0D"]);
+
+export const sunburstColorScheme = null;
